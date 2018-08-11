fbace5f6c1bb90e45b5fb69c264ad99e441d0cc3
sp.sankar@icloud.com <sp@spsankaricloudcoms-MacBook-Pro.local>
2018-08-11 04:31:11 -0400

    Initial Commit

---
+++ Swift funTests/Swift_funTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_funTests.swift
+//  Swift funTests
+//
+//  Created by sp.sankar@icloud.com on 8/11/18.
+//  Copyright © 2018 qorigin. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_fun
+
+class Swift_funTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
